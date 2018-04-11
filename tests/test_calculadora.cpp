a56107f30ba1bc87bfaf64f4c2d0f24c47cd9dd1
ToOrtner <tortner@dc.uba.ar>
2018-04-11 12:56:11 -0300

    Mas tests

--- tests/test_calculadora.cpp
+++ tests/test_calculadora.cpp
@@ -31,8 +31,38 @@ TEST (test_calculadora, gral2){
     c.ejecutar("MAIN");
     EXPECT_EQ(c.valorVariable("z"), 17);
     p.agregarInstruccion("MAIN", Instruccion(PUSH, 2));
-    p.agregarInstruccion("MAIN", Instruccion(ADD));
+    p.agregarInstruccion("MAIN", Instruccion(MUL));
     p.agregarInstruccion("MAIN", Instruccion(WRITE, "x"));
     c.ejecutar("MAIN");
-    EXPECT_EQ(c.valorVariable("x"), 7);
+    EXPECT_EQ(c.valorVariable("x"), 10);
+}
+TEST (test_calculadora, vacio){
+    Programa p;
+    Calculadora c(p);
+    c.asignarVariable("x", 12);
+    c.asignarVariable("y", 5);
+    c.asignarVariable("z", 4);
+    c.asignarVariable("w", 5);
+    p.agregarInstruccion("main", Instruccion(WRITE, "x"));
+    EXPECT_EQ(c.valorVariable("x"), 0);
+    p.agregarInstruccion("main", Instruccion(ADD));
+    p.agregarInstruccion("main", Instruccion(WRITE, "y"));
+    EXPECT_EQ(c.valorVariable("y"), 0);
+    p.agregarInstruccion("main", Instruccion(MUL));
+    p.agregarInstruccion("main", Instruccion(WRITE, "z"));
+    EXPECT_EQ(c.valorVariable("z"), 0);
+    p.agregarInstruccion("main", Instruccion(SUB));
+    p.agregarInstruccion("main", Instruccion(WRITE, "w"));
+    EXPECT_EQ(c.valorVariable("w"), 0);
+}
+TEST(test_calculadora, jumps){
+    Programa p;
+    Calculadora c(p);
+    c.asignarVariable("x", 3);
+    p.agregarInstruccion("main", Instruccion(PUSH, 1));
+    p.agregarInstruccion("main", Instruccion(SUB));
+    p.agregarInstruccion("main", Instruccion(WRITE, "x"));
+    p.agregarInstruccion("main", Instruccion(READ, "x"));
+    p.agregarInstruccion("main", Instruccion(JUMPZ, 0));
+    p.agregarInstruccion("main", Instruccion(JUMP, "main"));
 }
